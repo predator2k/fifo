0886d9afb66e93281214289893a1893242f32057
MH Nie <mhnie2000@outlook.com>
2024-10-30 19:46:33 +0800

    update

--- rtl/verilog/fifo.v
+++ rtl/verilog/fifo.v
@@ -1,84 +1,77 @@
-/******************************************************************************
- This Source Code Form is subject to the terms of the
- Open Hardware Description License, v. 1.0. If a copy
- of the OHDL was not distributed with this file, You
- can obtain one at http://juliusbaxter.net/ohdl/ohdl.txt
+`resetall
+`timescale 1ns / 1ps
+`default_nettype none
 
- Description: Store buffer
- Currently a simple single clock FIFO, but with the ambition to
- have combining and reordering capabilities in the future.
-
- Copyright (C) 2013 Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
-
- ******************************************************************************/
 
 module fifo
-  #(
+#(
     parameter DEPTH_WIDTH = 0,
     parameter DATA_WIDTH = 0
-    )
-   (
-    input 		    clk,
-    input 		    rst,
+) 
+(
+    input  wire                   clk,
+    input  wire                   rst_n,
 
-    input [DATA_WIDTH-1:0]  wr_data_i,
-    input 		    wr_en_i,
+    input  wire [DATA_WIDTH-1:0]  wr_data_i,
+    input  wire                   wr_en_i,
 
-    output [DATA_WIDTH-1:0] rd_data_o,
-    input 		    rd_en_i,
+    output wire [DATA_WIDTH-1:0]  rd_data_o,
+    input  wire                   rd_en_i,
 
-    output 		    full_o,
-    output 		    empty_o
-    );
+    output wire                   full_o,
+    output wire                   empty_o
+);
 
-   localparam DW = (DATA_WIDTH  < 1) ? 1 : DATA_WIDTH;
-   localparam AW = (DEPTH_WIDTH < 1) ? 1 : DEPTH_WIDTH;
+localparam DW = (DATA_WIDTH  < 1) ? 1 : DATA_WIDTH;
+localparam AW = (DEPTH_WIDTH < 1) ? 1 : DEPTH_WIDTH;
 
-   //synthesis translate_off
-   initial begin
-      if(DEPTH_WIDTH < 1) $display("%m : Warning: DEPTH_WIDTH must be > 0. Setting minimum value (1)");
-      if(DATA_WIDTH < 1) $display("%m : Warning: DATA_WIDTH must be > 0. Setting minimum value (1)");
-   end
-   //synthesis translate_on
+initial begin
+    if(DEPTH_WIDTH < 1)  begin
+        $error("%m : DEPTH_WIDTH must be > 0");
+        $finish;
+    end
+    if(DATA_WIDTH < 1) begin
+        $error("%m : DATA_WIDTH must be > 0");
+        $finish;
+    end
+end
 
-   reg [AW:0] write_pointer;
-   reg [AW:0] read_pointer;
+reg [AW:0] write_pointer;
+reg [AW:0] read_pointer;
 
-   wire 	       empty_int = (write_pointer[AW] ==
-				    read_pointer[AW]);
-   wire 	       full_or_empty = (write_pointer[AW-1:0] ==
-					read_pointer[AW-1:0]);
-   
-   assign full_o  = full_or_empty & !empty_int;
-   assign empty_o = full_or_empty & empty_int;
-   
-   always @(posedge clk) begin
-      if (wr_en_i)
-	write_pointer <= write_pointer + 1'd1;
+wire empty_int = (write_pointer[AW] == read_pointer[AW]);
+wire full_or_empty = (write_pointer[AW-1:0] == read_pointer[AW-1:0]);
 
-      if (rd_en_i)
-	read_pointer <= read_pointer + 1'd1;
+assign full_o  = full_or_empty & !empty_int;
+assign empty_o = full_or_empty & empty_int;
 
-      if (rst) begin
-	 read_pointer  <= 0;
-	 write_pointer <= 0;
-      end
-   end
-   simple_dpram_sclk
-     #(
-       .ADDR_WIDTH(AW),
-       .DATA_WIDTH(DW),
-       .ENABLE_BYPASS(1)
-       )
-   fifo_ram
-     (
-      .clk			(clk),
-      .dout			(rd_data_o),
-      .raddr			(read_pointer[AW-1:0]),
-      .re			(rd_en_i),
-      .waddr			(write_pointer[AW-1:0]),
-      .we			(wr_en_i),
-      .din			(wr_data_i)
-      );
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+        read_pointer  <= 0;
+        write_pointer <= 0;
+    end else begin
+        if (wr_en_i) begin
+            write_pointer <= write_pointer + 1'd1;
+        end
+        if (rd_en_i) begin
+            read_pointer <= read_pointer + 1'd1;
+        end
+    end
+end
+
+simple_dpram_sclk
+#(
+    .ADDR_WIDTH(AW),
+    .DATA_WIDTH(DW),
+    .ENABLE_BYPASS(1)
+) fifo_ram (
+    .clk(clk),
+    .dout(rd_data_o),
+    .raddr(read_pointer[AW-1:0]),
+    .re(rd_en_i),
+    .waddr(write_pointer[AW-1:0]),
+    .we(wr_en_i),
+    .din(wr_data_i)
+);
 
 endmodule
--- rtl/verilog/simple_dpram_sclk.v
+++ rtl/verilog/simple_dpram_sclk.v
@@ -13,50 +13,53 @@
  ******************************************************************************/
 
 module simple_dpram_sclk
-  #(
+#(
     parameter ADDR_WIDTH = 32,
     parameter DATA_WIDTH = 32,
     parameter ENABLE_BYPASS = 1
-    )
-   (
-    input 		    clk,
-    input [ADDR_WIDTH-1:0]  raddr,
-    input 		    re,
-    input [ADDR_WIDTH-1:0]  waddr,
-    input 		    we,
-    input [DATA_WIDTH-1:0]  din,
-    output [DATA_WIDTH-1:0] dout
-    );
+)
+(
+    input  wire                  clk,
 
-   reg [DATA_WIDTH-1:0]     mem[(1<<ADDR_WIDTH)-1:0];
-   reg [DATA_WIDTH-1:0]     rdata;
+    input  wire [ADDR_WIDTH-1:0] raddr,
+    input  wire                  re,
+    output wire [DATA_WIDTH-1:0] dout,
+
+    input  wire [ADDR_WIDTH-1:0] waddr,
+    input  wire                  we,
+    input  wire [DATA_WIDTH-1:0] din
+);
+
+reg [DATA_WIDTH-1:0]     mem[(1<<ADDR_WIDTH)-1:0];
+reg [DATA_WIDTH-1:0]     rdata;
 
 generate
-if (ENABLE_BYPASS) begin : bypass_gen
-   reg [DATA_WIDTH-1:0]     din_r;
-   reg 			    bypass;
+    if (ENABLE_BYPASS) begin : bypass_gen
+        reg [DATA_WIDTH-1:0] din_r;
+        reg  bypass;
 
-   assign dout = bypass ? din_r : rdata;
+        assign dout = bypass ? din_r : rdata;
 
-   always @(posedge clk)
-     if (re)
-       din_r <= din;
+        always @(posedge clk)
+            if (re)
+                din_r <= din;
 
-   always @(posedge clk)
-     if (waddr == raddr && we && re)
-       bypass <= 1;
-     else if (re)
-       bypass <= 0;
-end else begin
-   assign dout = rdata;
-end
+        always @(posedge clk)
+            if (waddr == raddr && we && re)
+                bypass <= 1;
+            else if (re)
+                bypass <= 0;
+
+    end else begin
+        assign dout = rdata;
+    end
 endgenerate
 
-   always @(posedge clk) begin
-      if (we)
-	mem[waddr] <= din;
-      if (re)
-	rdata <= mem[raddr];
-   end
+always @(posedge clk) begin
+    if (we)
+        mem[waddr] <= din;
+    if (re)
+        rdata <= mem[raddr];
+end
 
 endmodule
